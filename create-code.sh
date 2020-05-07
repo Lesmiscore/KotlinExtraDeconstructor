f0e645d266f01bf1eaacf588a05f03916a0f8804
nao20010128nao <nao20010128@gmail.com>
2020-05-07 01:35:36 +0000

    インデックスが間違ってた

--- create-code.sh
+++ create-code.sh
@@ -3,10 +3,10 @@
 _code(){
   echo 'package com.nao20010128nao.ked'
   echo
-  seq 5 100 | while read r ; do echo "inline operator fun <T> List<T>.component$r(): T = this[$r]" ; done
+  seq 5 200 | while read r ; do echo "inline operator fun <T> List<T>.component$r(): T = this[$((r-1))]" ; done
   echo
-  seq 5 100 | while read r ; do echo "inline operator fun <T> Array<T>.component$r(): T = this[$r]" ; done
+  seq 5 200 | while read r ; do echo "inline operator fun <T> Array<T>.component$r(): T = this[$((r-1))]" ; done
   echo
 }
 
-_code > src/main/kotlin/com/nao20010128nao/ked/code.kt
+_code | tee src/main/kotlin/com/nao20010128nao/ked/code.kt
